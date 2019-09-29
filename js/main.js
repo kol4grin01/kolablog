f3610d4ffdfea5b13da4ff6e3c4b61ae051b731d
BRACH <kol4grin01@gmail.com>
2019-09-29 21:14:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*smooth scroll effect*/
+$(function() {
+    $("a.smooth-scroll").click(function(event){
+        event.preventDefault();
+
+        //get/return id like #about, #home, etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 2000);
+    });
+});
+
+/*Hide navbar on scroll*/
+let prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+    let currentScrollPos = window.pageYOffset;
+    if(prevScrollPos > currentScrollPos) {
+        document.querySelector('.navbar').style.top = '0';
+    } else {
+        document.querySelector('.navbar').style.top = '-60px';
+    }
+    prevScrollPos = currentScrollPos;
+}
